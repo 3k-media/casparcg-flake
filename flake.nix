6fb661a874ec2e8a01b1401bb552d34c2badccb3
Leon Kamm <4458913+xanecs@users.noreply.github.com>
2025-07-25 11:44:09 +0200

    fix: build custom cef package

--- flake.nix
+++ flake.nix
@@ -12,16 +12,31 @@
       ];
       systems = ["x86_64-linux"];
       perSystem = { system, config, pkgs, ... }:
-        let
-          cef = pkgs.libcef.overrideAttrs (oldAttrs: {
+        {
+          packages.casparcg-cef = pkgs.cef-binary.overrideAttrs (oldAttrs: {
             version = "131.4.1";
             __intentionallyOverridingVersion = true;
             gitRevision = "437feba";
             chromiumVersion = "131.0.6778.265";
             srcHash = { x86_64-linux = "sha256-vTBHBJWFwbebzqGFGHEYH0uenLygWoOJW6kLcFSnQIM="; }.${system};
+            installPhase = ''
+                runHook preInstall
+
+                mkdir -p $out/lib
+                cp libcef_dll_wrapper/libcef_dll_wrapper.a $out/lib/
+                cp ../${oldAttrs.buildType}/libcef.so $out/lib/
+                cp ../${oldAttrs.buildType}/libEGL.so $out/lib/
+                cp ../${oldAttrs.buildType}/libGLESv2.so $out/lib/
+                cp ../${oldAttrs.buildType}/libvk_swiftshader.so $out/lib/
+                cp ../${oldAttrs.buildType}/libvulkan.so.1 $out/lib/
+                cp ../${oldAttrs.buildType}/chrome-sandbox $out/lib/
+                cp ../${oldAttrs.buildType}/*.bin ../${oldAttrs.buildType}/*.json $out/lib/
+                cp -r ../Resources/* $out/lib/
+                cp -r ../include $out/
+
+                runHook postInstall
+            '';
           });
-        in
-        {
           packages.default = config.packages.casparcg-server;
           packages.casparcg-server = pkgs.stdenv.mkDerivation rec {
             pname = "casparcg-server";
@@ -53,20 +68,15 @@
               simde
               zlib
               icu
-            ] ++ [ cef ];
+            ] ++ [ config.packages.casparcg-cef ];
 
             cmakeFlags = [
               "-DUSE_STATIC_BOOST=0"
               "-DUSE_SYSTEM_FFMPEG=1"
               "-DENABLE_HTML=1"
               "-DUSE_SYSTEM_CEF=1"
-              "-DCEF_ROOT=${cef}"
+              "-DCEF_ROOT=${config.packages.casparcg-cef}"
             ];
-
-            postInstall = ''
-              mkdir -p $out/bin/
-              cp -r ${cef}/share/cef/* $out/bin/
-            '';
 
             patches = [
               ./cmake-cef.patch
